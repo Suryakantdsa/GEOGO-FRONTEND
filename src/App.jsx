c4a2ad3ae9f3d6bf2d091ad3866351f294b660ee
Suryakantdsa <dassuryakantadas@gmail.com>
2023-08-27 00:20:49 +0530

    first commit as all setup is done ??

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="">
+       <h1 className='p-1 m-2 bg-red-400 text-white font-bold text-4xl'>this my name ...?</h1>
+      </div>
+    
+    </>
+  )
+}
+
+export default App
